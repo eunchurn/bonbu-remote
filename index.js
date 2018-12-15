ec37cc728101963a9530ade4b31a781cf272ce95
eunchurn <eunchurn.park@gmail.com>
2018-12-15 12:03:08 +0900

    node-lirc

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ nodeLIRC.on('stdout', (event) => {
 	if (event.eventName == 'EVENT_BUTTON_NAME')
 		nodeLIRC.writeLine('VOLUME_UP');
 });
+
+nodeLIRC.on('exit', function(code) {
+	console.log('irrecord exited with code ' + (code?code.toString():'(unknown)'));
+});
+
+nodeLIRC.on('remote-config-ready', function(remoteConfig) {
+        console.log(remoteConfig.name + " remote is READY.");
+        console.log("Configuration file data:\n" + remoteConfig.configuration);
+        // Insert or Update the new remote configuration into lirc config file
+        nodeLIRC.upsertRemote(remoteConfig.name, remoteConfig.configuration, (error) => {
+        	if (!error)
+            	nodeLIRC.reloadData();
+        });
+    });
+
+// Start the remote control recording process
+nodeLIRC.record('<MY_NEW_REMOTE_NAME>');
