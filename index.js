1d5bbd1ef6cf2c644c1137766234093a236500c9
eunchurn <eunchurn.park@gmail.com>
2018-12-15 14:02:34 +0900

    test

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const lirc_node = require('lirc_node')
 
 lirc_node.init();
-lirc_node.addListener((data) => {
+console.log(lirc_node.remotes);
+
+const listenerId = lirc_node.addListener((data) => {
     console.log(`Received ${data.key} from remote ${data.remote}`);
-    setInterval(() => {
-        console.log(`Sending ${data.key} to remote Bonbu`);
-        lirc_node.irsend.send_once('bonbu-remote', data.key);
-    },2000)
     if (data.remote == 'bonbu-remote') {
         lirc_node.irsend.send_once('bonbu-remote', data.key);
         console.log(`Sending ${data.key} to remote Bonbu`);
     }
 });
 
+setInterval(() => {
+    console.log(`Sending ${data.key} to remote Bonbu`);
+    lirc_node.irsend.send_once('bonbu-remote', data.key);
+},2000)
