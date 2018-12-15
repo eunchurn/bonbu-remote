3037fcc22462a638d1695848423b27c164655c00
eunchurn <eunchurn.park@gmail.com>
2018-12-15 13:55:44 +0900

    interval test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ lirc_node.init();
 lirc_node.addListener((data) => {
     console.log(`Received ${data.key} from remote ${data.remote}`);
     setInterval(() => {
+        console.log(`Sending ${data.key} to remote Bonbu`);
         lirc_node.irsend.send_once('bonbu-remote', data.key);
     },2000)
     if (data.remote == 'bonbu-remote') {
