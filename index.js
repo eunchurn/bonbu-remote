225a0c2c8c05f23c032387e325f113fb7aa0934a
eunchurn <eunchurn.park@gmail.com>
2018-12-15 13:24:50 +0900

    interval test

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const lirc_node = require('lirc_node')
 lirc_node.init();
 lirc_node.addListener((data) => {
     console.log(`Received ${data.key} from remote ${data.remote}`);
+    setInterval(() => {
+        lirc_node.irsend.send_once('bonbu-remote', data.key);
+    },2000)
     if (data.remote == 'bonbu-remote') {
         lirc_node.irsend.send_once('bonbu-remote', data.key);
         console.log(`Sending ${data.key} to remote Bonbu`);
     }
 });
 
-setInterval(() => {
-    lirc_node.irsend.send_once('bonbu-remote', 'KEY_POWER');
-},2000)
