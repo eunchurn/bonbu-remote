4526961218044a4bc4b4c84fe0e1bdac5f3ed114
eunchurn <afextwin@gmail.com>
2018-12-15 21:31:27 +0900

    start

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ lirc_node.init();
 console.log(lirc_node.remotes);
 
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.send('hello Bonbu deZentral');
 });
 
 app.get('/airconon', (req, res) => {
@@ -25,17 +25,17 @@ app.listen(8080);
 
 
 
-const listenerId = lirc_node.addListener((data) => {
-    console.log(`Received ${data.key} from remote ${data.remote}`);
-    if (data.remote == 'bonbu-remote') {
-        lirc_node.irsend.send_once('bonbu-remote', data.key);
-        console.log(`Sending ${data.key} to remote Bonbu`);
-    }
-});
+// const listenerId = lirc_node.addListener((data) => {
+//     console.log(`Received ${data.key} from remote ${data.remote}`);
+//     if (data.remote == 'bonbu-remote') {
+//         lirc_node.irsend.send_once('bonbu-remote', data.key);
+//         console.log(`Sending ${data.key} to remote Bonbu`);
+//     }
+// });
 
-setInterval(() => {
-    // console.log(`Sending ${data.key} to remote Bonbu`);
-    lirc_node.irsend.send_once('bonbu-remote', 'KEY_POWER', () => {
-        console.log("Sent bonbu-remote power command!");
-    });
-},2000)
+// setInterval(() => {
+//     // console.log(`Sending ${data.key} to remote Bonbu`);
+//     lirc_node.irsend.send_once('bonbu-remote', 'KEY_POWER', () => {
+//         console.log("Sent bonbu-remote power command!");
+//     });
+// },2000)
